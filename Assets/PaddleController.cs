23d8af70b2f1adbc85bfefd1c255e77b83b006b4
Eli <elizatelkamp@gmail.com>
2024-03-26 12:53:41 -0400

    Added PaddleController

--- Assets/PaddleController.cs
+++ Assets/PaddleController.cs
@@ -1,18 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float paddleSpeed = 1f;
+    private Vector3 playerPos = new Vector3(0, -9.5f, 0);
     void Update()
     {
-        
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        playerPos = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0f);
+        transform.position = playerPos;
     }
 }
