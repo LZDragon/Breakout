8db5d7d995b3fc2e461965b749b548c6764e0c47
Eli <elizatelkamp@gmail.com>
2024-03-28 09:50:59 -0400

    Added header

--- Assets/Scripts/SceneLoaded.cs
+++ Assets/Scripts/SceneLoaded.cs
@@ -1,3 +1,11 @@
+//////////////////////////////////////////////
+//Assignment/Lab/Project: Breakout
+//Name: Eliza Majernik
+//Section: SGD.213.2172
+//Instructor: Brian Sowers
+//Date: 04/01/2024
+/////////////////////////////////////////////
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
